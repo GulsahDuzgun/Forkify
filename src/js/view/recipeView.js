faf968d25abdf3a1f6693d42bbdca119d9a627a0
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-04-16 15:00:26 +0300

    Update recipeView.js

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -1,6 +1,6 @@
 import icons from '../../img/icons.svg';
 
-import { _, Fraction } from '../../../node_modules/fractional/index';
+import { _, Fraction } from 'fractional';
 import View from './View';
 import { API_KEY } from '../config';
 
